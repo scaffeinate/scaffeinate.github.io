4e98d6e086be23090877fcec2eccd58e852a83f7
Sudharsanan <sudhar.ti@gmail.com>
2015-01-24 01:25:56 -0600

    Update content

--- js/util.js
+++ js/util.js
@@ -1,10 +1,19 @@
-function scroll(element) {
+function scrollDiv(element) {
   $("html, body").delay(100).animate({ scrollTop: $(element).offset().top }, 900);
 };
 
 $(document).ready(function(){
   $(".scroll-link").click(function(e){
     e.preventDefault();
-    scroll($(this).attr("href"));
+    scrollDiv($(this).attr("href"));
   });
+
+  $(window).on('scroll', function(){
+    var topofDiv = $("#skills").offset().top;
+    var height = $("#skills").outerHeight();
+    if($(window).scrollTop() > (topofDiv - height)){
+      $('.progress .progress-bar').progressbar();
+    }
+  });
+
 });
