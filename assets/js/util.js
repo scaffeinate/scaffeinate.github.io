28faebb98bc9ab5d6bfd302b9e5a990740a59fa1
Sudharsanan <sudhar.ti@gmail.com>
2015-06-23 20:36:54 -0500

    Projects section

--- assets/js/util.js
+++ assets/js/util.js
@@ -37,4 +37,21 @@ $(document).ready(function(){
     }
   });
 
+  var owl = $("#projects-owl");
+ 
+  owl.owlCarousel({
+      items : 3, //10 items above 1000px browser width
+      itemsDesktop : [1000,2], //5 items between 1000px and 901px
+      itemsDesktopSmall : [900,2], // betweem 900px and 601px
+      itemsTablet: [600,2], //2 items between 600 and 0
+      itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+  });
+
+  $(".next").click(function(){
+    owl.trigger('owl.next');
+  })
+  $(".prev").click(function(){
+    owl.trigger('owl.prev');
+  })
+
 });
