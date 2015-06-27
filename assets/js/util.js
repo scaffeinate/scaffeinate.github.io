4226b226aab409e6d526925a541b8314f9e568db
Sudharsanan <sudhar.ti@gmail.com>
2015-06-27 13:05:45 -0500

    completed till showcase, need to cleanup css a bit

--- assets/js/util.js
+++ assets/js/util.js
@@ -1,27 +1,14 @@
-function scrollDiv(element) {
-  $("html, body").delay(250).animate({ scrollTop: $(element).offset().top }, 500, 'easeInCubic');
-};
-
 $(document).ready(function(){
 
   new WOW().init();
 
-  $("a[href*=#]").click(function(e){
-    e.preventDefault();
-    scrollDiv($(this).attr("href"));
-  });
-
   $("#phone").popover();
 
-  var owl = $("#projects-owl");
+  var owl = $("#showcase-owl");
 
   owl.owlCarousel({
-      items : 3, //10 items above 1000px browser width
-      itemsDesktop : [1000,2], //5 items between 1000px and 901px
-      itemsDesktopSmall : [900,2], // betweem 900px and 601px
-      itemsTablet: [600,2], //2 items between 600 and 0
-      itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
-    });
+    items : 3
+  });
 
   $(".next").click(function(){
     owl.trigger('owl.next');
