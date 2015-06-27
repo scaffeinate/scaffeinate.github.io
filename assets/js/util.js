87542d5f44d53176863ce2b6ab5e2970113c17cc
Sudharsanan <sudhar.ti@gmail.com>
2015-06-27 15:25:12 -0500

    Add parse, contact form mail

--- assets/js/util.js
+++ assets/js/util.js
@@ -1,20 +1,25 @@
 $(document).ready(function(){
 
+  Parse.initialize("8wZ35cV3Yl6ppfcNDzDDyVFCPxP00JT6npenQ14z", "PRMy5Vzq3F0M8PURVoanaFH6YtOsykgdvull1Q2T");
+
+  $("#contact-form").submit(function(e){
+    e.preventDefault();
+    var data = $(this).serialize();
+
+    Parse.Cloud.run("sendMail", data, {
+      success: function(object) {
+        console.log("Sent");
+      },
+      error: function(object, error) {
+        console.log("Error" + error);
+      }
+    });
+  });
+
   new WOW().init();
 
-  $("#phone").popover();
-
   var owl = $("#showcase-owl");
-
   owl.owlCarousel({
     items : 3
-  });
-
-  $(".next").click(function(){
-    owl.trigger('owl.next');
-  });
-
-  $(".prev").click(function(){
-    owl.trigger('owl.prev');
   });
 });
